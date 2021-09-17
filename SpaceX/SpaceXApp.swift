020ed611f4b89965ea132e36b7a91575177ead3f
Kaedin Schouten <skaedin@gmail.com>
2021-09-17 17:26:47 +0200

    Initial Commit

---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Kaedin Schouten on 17/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
